0dedc6e46212e19bf48f96b32e02ac5c5b83ef55
HorseScary <jakobaayers@gmial.com>
2021-11-16 14:34:58 -0500

    cut 9 bytes off of 12 days

--- 12-days.py
+++ 12-days.py
@@ -2,5 +2,5 @@ d=['First','Second','Third','Fourth','Fifth','Sixth','Seventh','Eighth','Ninth',
 p=['A Partridge in a Pear Tree.\n','Two Turtle Doves, and','Three French Hens,','Four Calling Birds,','Five Gold Rings,','Six Geese-a-Laying,','Seven Swans-a-Swimming,','Eight Maids-a-Milking,','Nine Ladies Dancing,','Ten Lords-a-Leaping,','Eleven Pipers Piping,','Twelve Drummers Drumming,']
 for a in range(12):
     print(f'On the {d[a]} day of Christmas\nMy true love sent to me')
-    for i in range(a+1):
-        print(p[a-i])
+    for i in range(a+1):print(p[a-i])
+    
