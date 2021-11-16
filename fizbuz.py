da021384234114e2bc61c9d027ac8f4abc2c04ba
HorseScary <jakobaayers@gmial.com>
2021-11-16 14:39:48 -0500

    removed whitespace from fizzbuzz

--- fizbuz.py
+++ fizbuz.py
@@ -1,10 +1,8 @@
 from typing import Type
 for i in range(1,101):
     a=''
-    if i%5!=0 and i%3!=0:
-        a=i
-    if i%3==0:
-        a='Fizz'
+    if i%5!=0 and i%3!=0:a=i
+    if i%3==0:a='Fizz'
     if i%5==0:
         print(f'{a}Buzz')
         continue
