9dde1af026996dc68faefb6a05affd891949d409
HorseScary <jakobaayers@gmial.com>
2021-11-17 07:41:42 -0500

    bad js

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+console.log('penis')
+for(let i=0;i<100;i++){
+    a='';
+    if(i%5!=0&&i%3!=0){
+        a=1}
+    if(i%3==0){
+        (Fizz)
+    }
+}
+
+//for i in range(1,101):
+//a=''
+//if i%5!=0 and i%3!=0:a=i
+//if i%3==0:a='Fizz'
+//if i%5==0:
+//    print(f'{a}Buzz')
+//    continue
+//print(a)
