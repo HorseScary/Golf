7af07074daeebab86d9fd31bcc0e8a71dd60dcc9
v <panagen3002@gmail.com>
2021-11-21 16:46:54 -0500

    working on smaller fizzbuzz

--- fizbuz.py
+++ fizbuz.py
@@ -1,8 +1,6 @@
 for i in range(1,101):
     a=''
-    if i%5!=0 and i%3!=0:a=i
-    if i%3==0:a='Fizz'
-    if i%5==0:
-        print(f'{a}Buzz')
-        continue
-    print(a)
+    if i%3==0:a=('Fizz')
+    if i%5==0:a=('Buzz')
+    try:int(a)
+    except ValueError:print(i)
